8abf7f4d474c9ef8fe4fe38a06e962f4f067127b
Monica Doan <mtvdoan@gmail.com>
2023-02-22 19:09:45 -0800

    Created react-app skeleton and install a lot of npms including tailwind in case and included some custom hooks like Boop for fun

--- PhotoBomb/server/server.js
+++ PhotoBomb/server/server.js
@@ -1,10 +1,25 @@
+//Dependencies
 const express = require("express"); 
 const cors = require("cors");
-const app = express();                
+const app = express();
+const cors = require('cors')
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const port = 8000;
+
+//Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin:"http://localhost:3000"
 }));
 
-app.listen(8000, ()=>console.log("Listening on Port 8000"))             
+//Requires
+require("./config/mongoose.config");
+require("./routes/SOMETHING.routes")(app);  //Need to edit 'SOMETHING' with real name. lol
+
+
+//Listen Method
+app.listen(port, ()=>console.log("Listening on port:", port))             
